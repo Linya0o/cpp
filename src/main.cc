3d3011b61499328d117483b043a06aa7b1abd240
linya0o <linya0o@163.com>
2022-12-20 00:17:26 +0800

    [√] chapter.3

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <cstdio>
 
 int main(int argc, char *argv[]) {
-    std::cout << "hello world" << std::endl;
+    unsigned short font {0x064C};
+    auto italic {static_cast<unsigned short>(1u << 6)};
+    auto bold {static_cast<unsigned short>(1u << 5)};
+
+    font |= italic | bold;
+    printf("font = 0x%X\n", font);
+    font &= ~bold;            // turn off bold
+    font &= ~bold & ~italic;  // turn bold and italic off
+    font &= ~(bold | italic);  // turn bold and italic off
+    printf("font = 0x%X\n", font);
+    font ^= bold;  // toggle bold
+    printf("font = 0x%X\n", font);
+#if 0
+    // 旧式枚举, 缺少class修饰
+    enum Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+
+    // 作用域的枚举，类型规范，输出转换
+    enum class Punctuation:char { Comma = ',', Exclamation = '!', Question = '?' };
+    Punctuation ch {Punctuation::Question};
+    std::cout << static_cast<char>(ch) << std::endl;
+
+    // 通过using声明绕过枚举的作用域要求
+    using enum Punctuation;
+    ch = Comma;
+
+    // using指令
+    using Bigones = unsigned long long;
+
+#endif // 0
 
     return 0;
 }
